36594fc5187a6be0d02980ec2e71c15b069a8c66
Ace Yogurt <jeremygill87@gmail.com>
2020-07-19 21:51:37 -0400

    Updates

---
+++ precache-manifest.61c9ab1a3008edb9a9375e3a536959ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435950d9f94dec3e7b4e52547f5df5ea",
+    "url": "/reacthangman/index.html"
+  },
+  {
+    "revision": "8434634ecab0c950ffdd",
+    "url": "/reacthangman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b02f7ae7e353709dcfd0",
+    "url": "/reacthangman/static/js/2.87f75564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthangman/static/js/2.87f75564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8434634ecab0c950ffdd",
+    "url": "/reacthangman/static/js/main.01366c4b.chunk.js"
+  },
+  {
+    "revision": "daf754103b387ef8728c",
+    "url": "/reacthangman/static/js/runtime-main.093917bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacthangman/static/media/logo.5d5d9eef.svg"
+  }
+]);
